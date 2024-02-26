497bad074009d723a9be82add20096123dde89bb
lucasmarques2907 <lucasmarques2907@gmail.com>
2024-02-26 01:40:39 -0300

    Separando arquivos css, ajustes nas animações da nav mobile

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-function perguntasFrequentes(){
-    window.location.href="perguntasFrequentes.php";
-}
-
-function navMobile(){
-    window.location.href="navMobile.php";
-}
-
 const pathname = window.location.pathname;
 
-if (pathname.includes("navMobile.php")){
+if (pathname.includes("index.php")) {
+
+    function perguntasFrequentes() {
+        window.location.href = "faq.php";
+    }
+
+    function navMobile() {
+        window.location.href = "navMobile.php";
+    }
+}
+if (pathname.includes("navMobile.php")) {
+
     home = document.querySelector("#home");
     work = document.querySelector("#work");
     blog = document.querySelector("#blog");
@@ -19,51 +22,51 @@ if (pathname.includes("navMobile.php")){
     blogButton = document.querySelector(".blogButton");
     aboutButton = document.querySelector(".aboutButton");
 
-    function selectHome(){
-        home.removeAttribute("class");
-        work.setAttribute("class", "d-none");
-        blog.setAttribute("class", "d-none");
-        about.setAttribute("class", "d-none");
+    function selectHome() {
+        home.setAttribute("class", "showImg");
+        work.setAttribute("class", "hideImg");
+        blog.setAttribute("class", "hideImg");
+        about.setAttribute("class", "hideImg");
 
         homeButton.classList.add("buttonPressed")
         workButton.classList.remove("buttonPressed");
         blogButton.classList.remove("buttonPressed");
         aboutButton.classList.remove("buttonPressed");
     }
-    
-    function selectWork(){
-        home.setAttribute("class", "d-none");
-        work.removeAttribute("class");
-        blog.setAttribute("class", "d-none");
-        about.setAttribute("class", "d-none");
-    
+
+    function selectWork() {
+        home.setAttribute("class", "hideImg");
+        work.setAttribute("class", "showImg");
+        blog.setAttribute("class", "hideImg");
+        about.setAttribute("class", "hideImg");
+
         homeButton.classList.remove("buttonPressed")
         workButton.classList.add("buttonPressed");
         blogButton.classList.remove("buttonPressed");
         aboutButton.classList.remove("buttonPressed");
     }
-    
-    function selectBlog(){
-        home.setAttribute("class", "d-none");
-        work.setAttribute("class", "d-none");
-        blog.removeAttribute("class");
-        about.setAttribute("class", "d-none");
+
+    function selectBlog() {
+        home.setAttribute("class", "hideImg");
+        work.setAttribute("class", "hideImg");
+        blog.setAttribute("class", "showImg");
+        about.setAttribute("class", "hideImg");
 
         homeButton.classList.remove("buttonPressed")
         workButton.classList.remove("buttonPressed");
         blogButton.classList.add("buttonPressed");
         aboutButton.classList.remove("buttonPressed");
     }
-    
-    function selectAbout(){
-        home.setAttribute("class", "d-none");
-        work.setAttribute("class", "d-none");
-        blog.setAttribute("class", "d-none");
-        about.removeAttribute("class");
+
+    function selectAbout() {
+        home.setAttribute("class", "hideImg");
+        work.setAttribute("class", "hideImg");
+        blog.setAttribute("class", "hideImg");
+        about.setAttribute("class", "showImg");
 
         homeButton.classList.remove("buttonPressed")
         workButton.classList.remove("buttonPressed");
         blogButton.classList.remove("buttonPressed");
         aboutButton.classList.add("buttonPressed");
-    }    
+    }
 }
